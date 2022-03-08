1b2fa9c3a5833f47355ba05b837e7905700d6e58
predicteev <predicteev.consulting@gmail.com>
2022-03-07 18:40:39 -0800

    addded classification notebook

---
+++ scripts_testicular_cancer/data_set (1).py
@@ -0,0 +1,76 @@
+
+# load dataset with image data generator
+from os import listdir
+from numpy import asarray
+from numpy import save
+import tensorflow
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.preprocessing.image import *
+from tensorflow.keras.optimizers import SGD
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+from numpy.random import seed
+from tensorflow.random import set_seed
+
+seed(1)
+set_seed(2)
+
+def initialize(train_path='/content/train/',
+          validation_path='/content/validation/',
+          test_path='/content/test/',
+          batch_size=64,
+          im_size = 200):
+  '''
+    creates data generators + class_map
+  '''
+
+  # create data generator
+  # TODO add data aug.
+  datagen = ImageDataGenerator(rescale=1.0/255.0)
+  
+  # prepare iterators(train,val,test)
+  train_it = datagen.flow_from_directory(train_path,
+    class_mode='categorical', batch_size=64, target_size=(im_size, im_size))
+  validation_it = datagen.flow_from_directory(validation_path,
+    class_mode='categorical', batch_size=64, target_size=(im_size, im_size))
+  test_it = datagen.flow_from_directory(test_path,
+    class_mode='categorical', batch_size=64, target_size=(im_size, im_size))
+  
+  # define class map
+  class_map = {v:k for (k,v) in test_it.class_indices.items()}
+  return train_it,validation_it,test_it,class_map
+
+def repr_im_hist(im_path, title=""):
+  '''
+   creates histogram + image subplot
+  '''
+  fig,ax = plt.subplots(1,2)
+  plt.title(title)
+  im = plt.imread(im_path)
+  ax[0].hist(im.ravel())
+  ax[1].imshow(im)
+  plt.show()
+
+def safe_save(model, save_path):
+  '''
+    save the model to disk
+  '''
+  try:
+    model.save(save_path)
+  except Exception as e:
+    print(e)
+
+def plot_history(history):
+  '''
+    quick plot of the loss curves (train+validation)
+  '''
+  assert "accuracy" in history.history.keys()
+  assert "val_accuracy" in history.history.keys()
+
+  plt.title("loss curves")
+  plt.plot(history.history["accuracy"])
+  plt.plot(history.history["val_accuracy"])
+  plt.show()
---
+++ scripts_testicular_cancer/modelzoo (1).py
@@ -0,0 +1,48 @@
+# load dataset with image data generator
+from os import listdir
+from numpy import asarray
+from numpy import save
+import tensorflow
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.preprocessing.image import *
+from tensorflow.keras.optimizers import SGD
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+# define cnn model
+def define_model(n_classes=3):
+	'''
+		definition of a simple model
+	'''
+	model = Sequential()
+	model.add(Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(6, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(n_classes, activation='softmax'))
+	return model
+
+def compile_model(model, opt=None):
+  if opt == None:
+    opt = SGD(lr=0.001, momentum=0.9)
+  # compile model
+
+  model.compile(optimizer=opt, 
+  loss='categorical_crossentropy', 
+  metrics=['accuracy'])
+  return model
+
+
+def string_predict_single(model, im, class_map):
+
+  if im.shape[-1]>3:
+    im = im[:,:,:3]
+  if im.shape[0:2] != model.input_shape[1:3]:
+    im = cv2.resize(im,(200,200))
+
+  predictions = model.predict(np.expand_dims(im, axis=0))
+  pred = np.argmax(predictions)
+  string_class = class_map[pred]
+  return string_class, predictions
