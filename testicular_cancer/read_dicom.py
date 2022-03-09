31210ff283a78e758835f5de32cf58a7b4668fa5
Assan Sanogo <assansanogo@gmail.com>
2022-03-09 08:26:02 -0800

    Update read_dicom.py

--- testicular_cancer/read_dicom.py
+++ testicular_cancer/read_dicom.py
@@ -2,7 +2,7 @@ import pydicom
 
 def create_dataset(file_path):
     '''
-    
+    function which creates dataset with LittleEndian format (necessary for truncated DICOM files)
     '''
 
     im_dataset = pydicom.dcmread(file_path, force=True)
