45f7d2ad413d52c1c834589378527b35dd99a685
Assan Sanogo <assansanogo@gmail.com>
2022-03-09 08:31:07 -0800

    update to add reference to environment files

--- testicular_cancer/config.py
+++ testicular_cancer/config.py
@@ -1,14 +1,24 @@
-# list all dicom/images (full_path)
-IMAGE_PATH = "/Users/assansanogo/Downloads/testis/*.png"
-INPUT_PATH = "/Users/assansanogo/Downloads/testis"
+import os
 
-DICOM_PATH = "/Users/assansanogo/Downloads/TGNS/DICOM"
-MASK_DICOM_PATH = "/Users/assansanogo/Downloads/TGNS/contours"
-DEST = "/Users/assansanogo/Downloads/patches"
+# list all dicom/images (full_path)
+
+# HOME path 
+HOME = os.getenv("TEST_CANCER_HOME", "/Users/assansanogo/Downloads")
+
+# binarized images
+IMAGE_PATH = os.path.join(HOME,"testis/*.png")
+INPUT_PATH = os.path.join(HOME,"testis")
+# original DICOMS               
+DICOM_PATH = os.path.join(HOME,"TGNS/DICOM")
+MASK_DICOM_PATH = os.path.join(HOME,"/TGNS/contours")
+DEST = os.path.join(HOME, "patches")
 
 # mapping (True:pathologic  False:non pathologic)
 patho_map = {True: "patho",
         False: "non-patho"}
+
+# initial flag
 patho = False
 
+# set dcm to None
 dcm = None
